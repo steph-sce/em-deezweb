a6fe3670468a762f2bd0c0fcb120764174f1c61f
steph28 <stephane.soucheyre@gmail.com>
2018-07-17 22:46:34 +0200

    ADD axios

--- src/views/accueil.vue
+++ src/views/accueil.vue
@@ -1,22 +1,38 @@
 <template>
   <div id="app">
     <h1>Accueil</h1>
+    <input type="text" v-model="message">
+    <button @click="search(message)">Recherche</button>
   </div>
 </template>
 
 <script>
-
+import axios from "axios";
 export default {
-  name: 'app',
-  components: {
+  name: "app",
+  data(){
+    return {
+      message: '',
+      artist: response.data.data
+    }
+  },
+  methods: {
+    search: function(message) {
+      axios
+        .get(
+          `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=artist:"${message}"&output=json`
+        )
+        .then(response => console.log(response.data.data));
+      console.log("YOLO");
+    }
   }
-}
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Montserrat');
+@import url("https://fonts.googleapis.com/css?family=Montserrat");
 
 #app {
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
 }
 </style>
