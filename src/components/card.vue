9bdd58baf48b6cfbcba5d3878974b56d960fca92
steph28 <stephane.soucheyre@gmail.com>
2018-07-18 11:55:27 +0200

    récupération data de l'appel get

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,25 +1,25 @@
 <template>
 <div>
   <b-card
-  img-src="https://picsum.photos/600/300/?image=25"
-  img-alt="Image"
-  img-top
-  tag="article"
-  style="max-width: 20rem;"
-  class="mb-2">
-  <p class="card-text">Titre de la chanson</p>
-  <p class="card-text">Artiste de la chanson</p>
-  <p class="card-text">Durer de la chanson</p>
-  <b-button href="#" variant="primary">Ecouter un extrait</b-button>
-  <b-button href="#" variant="primary">Consulter l'album</b-button>
-  <b-button href="#" variant="primary">Voir la fiche de l'artiste</b-button>
+    :img-src= "result.album.cover_xl"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+    >
+    <p class="card-text"></p>
+    <p class="card-text"></p>
+    <p class="card-text"></p>
+    <b-button href="#" variant="primary">Ecouter un extrait</b-button>
+    <b-button href="#" variant="primary">Consulter l'album</b-button>
+    <b-button href="#" variant="primary">Voir la fiche de l'artiste</b-button>
   </b-card>
 </div>
-
-<!-- card-img-1.vue -->
 </template>
 <script>
-export default {};
+export default {
+};
 </script>
 <style>
 </style>
--- src/views/accueil.vue
+++ src/views/accueil.vue
@@ -1,30 +1,51 @@
 <template>
   <div id="app">
     <h1>Accueil</h1>
-    <input type="text" v-model="message">
-    <button @click="search(message)">Recherche</button>
+    <input type="text" v-model="searchDeezer">
+    <button @click="search(searchDeezer)">Recherche</button>
+     <b-card
+    :img-src= "result.album.cover_xl"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+    v-for="result in results"
+    >
+    <p class="card-text">{{ result.title }}</p>
+    <p class="card-text">{{ result.artist.name }} / {{ result.album.title }}</p>
+    <p class="card-text">{{ result.duration }}</p>
+    <b-button href="#" variant="primary">Ecouter un extrait</b-button>
+    <b-button href="#" variant="primary">Consulter l'album</b-button>
+    <b-button href="#" variant="primary">Voir la fiche de l'artiste</b-button>
+  </b-card>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios'
+import Card from '../components/card'
 export default {
   name: "app",
-  data(){
+  data: function() {
     return {
-      message: '',
-      artist: response.data.data
-    }
+      searchDeezer: "",
+      results: []
+    };
   },
   methods: {
-    search: function(message) {
+    search: function(searchDeezer) {
       axios
         .get(
-          `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=artist:"${message}"&output=json`
+          `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=artist:"${searchDeezer}"&output=json`
         )
-        .then(response => console.log(response.data.data));
-      console.log("YOLO");
+        .then(response => {
+          this.results = response.data.data;
+        });
     }
+  },
+  components: {
+    Card
   }
 };
 </script>
